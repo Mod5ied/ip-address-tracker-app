c95d3964e78b3fdf61f71f58d114f6e22e7e167a
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2020-12-19 03:52:21 +0100

    FInished Design and layouting for desktop and mobile views

---
+++ main.js
@@ -0,0 +1,9 @@
+
+//make sure that input box has white background if empty
+const input = document.getElementById('input-box');
+
+let checkContent = () => {
+    if(input.value === "" && input.value === null ){
+        input.style.background
+    }
+}
