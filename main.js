5567969c08c78c6f4ff110e20685b4bb04af2e1d
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2020-12-29 23:25:32 +0100

    Update to main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+//define our constants
+const myKey = "";
+const searchField = document.getElementById("input-box");
 
-//make sure that input box has white background if empty
-const input = document.getElementById('input-box');
+//listen to click events of the keyboard
+const fireSearch = document.getElementById("submit-btn");
+fireSearch.addEventListener("click", (e) => {
+  console.log(searchField.value.toLowerCase());
+  
+  const URL = `https://geo.ipify.org/api/v1?apiKey={myKey}&ipAddress={searchField}`;
+  
+  //make an axios get request
+  axios
+    .get(URL)
+    .then((res) => {})
+    .error((err) => {
+      console.log(err);
+    });
+});
 
-let checkContent = () => {
-    if(input.value === "" && input.value === null ){
-        input.style.background
-    }
-}
+//create a function that appends the main-area divs with search results.
